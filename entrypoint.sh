8dc25a2f97aacef38512933f7bccff34a608d2db
asannou <hataori@gmail.com>
2017-01-29 11:57:56 +0900

    コンテナ起動時に occ upgrade を実行

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,10 @@ chown www-data:root $CONFIG $DATA
 ln -snf $CONFIG /var/www/nextcloud/config/
 ln -snf $DATA /var/www/nextcloud/
 
+occ() {
+  args="$@"
+  su - -s /bin/sh -c "/usr/local/bin/php /var/www/nextcloud/occ $args" www-data
+}
+
+occ status | grep -q '\- installed: true' && occ upgrade --no-app-disable --no-interaction
 exec "$@"
