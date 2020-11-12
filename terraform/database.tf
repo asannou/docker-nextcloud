4984405d9e5c59dbd600fcf8d9427f046f56a053
asannou <hataori@gmail.com>
2020-11-12 17:00:01 +0900

    RDS のストレージタイプに明示的に汎用 SSD を指定

--- terraform/database.tf
+++ terraform/database.tf
@@ -21,6 +21,7 @@ resource "aws_db_instance" "db" {
   engine_version = "${var.db_engine_version}"
   instance_class = "${var.db_instance_class}"
   allocated_storage = "${var.db_allocated_storage}"
+  storage_type = "gp2"
   username = "${var.db_username}"
   password = "${var.db_password}"
   port = 3306
