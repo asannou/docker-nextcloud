c719efebfa81a2a8570f1c25e0f7aadd3859cbf8
asannou <hataori@gmail.com>
2021-06-14 18:00:41 +0900

    Version 21.0.2
    SSO & SAML authentication 4.1.1
    https://docs.nextcloud.com/server/21/admin_manual/configuration_files/encryption_migration.html

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,6 +22,7 @@ then
   occ upgrade --no-interaction
   occ db:add-missing-indices
   occ db:add-missing-columns
+  occ db:add-missing-primary-keys
   occ db:convert-filecache-bigint --no-interaction
   occ config:system:set memcache.local --value='\OC\Memcache\APCu'
   occ config:system:set trusted_proxies 0 --value=10.0.0.0/8
@@ -29,6 +30,7 @@ then
   occ config:system:set trusted_proxies 2 --value=192.168.0.0/16
   occ config:system:set datadirectory --value=$DATA
   occ config:system:set simpleSignUpLink.shown --type=boolean --value=false
+  occ encryption:scan:legacy-format && occ config:system:set encryption.legacy_format_support --type=boolean --value=false
 fi
 
 exec "$@"
